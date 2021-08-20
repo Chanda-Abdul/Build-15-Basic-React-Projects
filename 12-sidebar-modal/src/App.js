605209818011aa3f7cf975b7874cd0ee5cdd04c3
Chanda Abdul <56906759+Chanda-Abdul@users.noreply.github.com>
2021-08-19 20:56:02 -0700

    Add files via upload

---
+++ 12-sidebar-modal/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Modal from './components/Modal'
+import Sidebar from './components/Sidebar'
+import Home from './components/Home'
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Modal/>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
